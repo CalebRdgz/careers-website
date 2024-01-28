3393994f7639751bb59a594b051bb8e6f8d30493
CalebRdgz <calebrodgz@gmail.com>
2024-01-28 16:47:19 -0600

    refactored home html template to use included file to show data to the user. Created a new API route to display JSON string using jsonify.

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Dallas, Texas',
+        'salary': '$70,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Austin, Texas',
+        'salary': '$90,000'
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': 'Remote',
+    },
+    {
+        'id': 4,
+        'title': 'Backend Engineer',
+        'location': 'Phoenix, Arizona',
+        'salary': '$120,000'
+    }
+]
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS, company_name='Caleb')
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
 
 print(__name__)
 
